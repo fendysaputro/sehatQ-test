5e08e2d4cdbd00c3a0918f103339b80a1a4cfb0b
phephen <vidis194@gmail.com>
2021-01-22 11:18:50 +0700

    add search in home fragment

--- app/src/main/java/id/phephen/sehatq_test/ui/home/HomeFragment.kt
+++ app/src/main/java/id/phephen/sehatq_test/ui/home/HomeFragment.kt
@@ -1,20 +1,55 @@
 package id.phephen.sehatq_test.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import id.phephen.sehatq_test.R
+import id.phephen.sehatq_test.databinding.FragmentHomeBinding
+import id.phephen.sehatq_test.ui.search.SearchActivity
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var lytSearch: LinearLayout
+    private lateinit var progressBar: ProgressBar
+
+    private fun initView() {
+        lytSearch = binding.lytSearch
+        progressBar = binding.progressBar
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initialize()
+    }
+
+    private fun initialize() {
+        onLayoutSearchClicked()
+    }
+
+    private fun onLayoutSearchClicked() {
+        lytSearch.setOnClickListener {
+            val intent = Intent(context, SearchActivity::class.java)
+            startActivityForResult(intent, 88)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/id/phephen/sehatq_test/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package id.phephen.sehatq_test.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.phephen.sehatq_test.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
