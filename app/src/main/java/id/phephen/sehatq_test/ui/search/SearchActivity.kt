11b7a0c4fd5dfa6f8b854bc86c69bc81fe2bee86
phephen <vidis194@gmail.com>
2021-01-22 14:27:06 +0700

    update activity SearchActivity.kt

--- app/src/main/java/id/phephen/sehatq_test/ui/search/SearchActivity.kt
+++ app/src/main/java/id/phephen/sehatq_test/ui/search/SearchActivity.kt
@@ -2,11 +2,26 @@ package id.phephen.sehatq_test.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.phephen.sehatq_test.R
+import android.widget.EditText
+import android.widget.LinearLayout
+import id.phephen.sehatq_test.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+    private lateinit var lytSearch: LinearLayout
+    private lateinit var etsearch: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        initView()
+    }
+
+    private fun initView() {
+        lytSearch = binding.lytSearch
+        etsearch = binding.etSearch
     }
 }
